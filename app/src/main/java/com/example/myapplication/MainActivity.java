752bd844e77d53a63ab431260a4be6021c80c2c2
mohibqureshi <mohibqureshi2@gmail.com>
2020-06-05 14:53:52 +0530

    added an exit button clicking which pops up a message "I am too young to exit" and exits the application.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,14 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    public void finish() {
+
+        toast();
+        super.finish();
+
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -50,11 +58,22 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+
+        Button exit = (Button) findViewById(R.id.exitt);
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+
+
+        });
         }
 
 
-//    public void toast() {
-//        Toast.makeText(this, "This is a toast", Toast.LENGTH_LONG).show();
-//
-//    }
+    public void toast() {
+        Toast.makeText(this, "I am too young to exit", Toast.LENGTH_LONG).show();
+
+    }
 }
